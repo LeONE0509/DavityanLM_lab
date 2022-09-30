d3c1c9398ee9d786084dccbde8010ad48a8e22bf
LeONE0509 <freedomlove6574@gmail.com>
2022-10-01 01:05:12 +0300

    deleted comments from Main.java

--- src/main/java/tech/reliab/course/davityanlm/bank/Main.java
+++ src/main/java/tech/reliab/course/davityanlm/bank/Main.java
@@ -13,31 +13,31 @@ public class Main {
 
 
        BankOffice myBankOffice = new BankOffice(myBank, "Отделение №1", "улица Пушкина дом Колотушкина");
-//        myBankOffice.addAtm(myBankOffice);
-//        System.out.println(myBankOffice);
-//
-//        Employee Vechost = new Employee("Вечность Лев", new Date(), myBank, myBankOffice, "Генеральный");
-//        Vechost.setSalary(228322);
-//        System.out.println(Vechost);
-//
-//        BankAtm myBankAtm = new BankAtm("Samsung", myBank, myBankOffice, Vechost);
-//        myBankAtm.addMoney(myBankAtm, 228322);
-//        System.out.println(myBankAtm);
-//
-//        User Leva = new User("Вечность Лев", new Date(), "LevaBank228", myBank);
-//
-//        PaymentAccount pa = new PaymentAccount(Leva, myBank);
-//        pa.addMoney(pa, 228322);
-//        System.out.println(pa);
-//
-//        CreditAccount ca = new CreditAccount(Leva, myBank, Vechost, pa);
-//        ca.setCreditSum(1337);
-//        System.out.println(ca);
-//
-//
-//        Leva.setPaymentAccount(pa);
-//        Leva.setCreditAccount(ca);
-//        System.out.println(Leva);
+        myBankOffice.addAtm(myBankOffice);
+        System.out.println(myBankOffice);
+
+        Employee Vechost = new Employee("Вечность Лев", new Date(), myBank, myBankOffice, "Генеральный");
+        Vechost.setSalary(228322);
+        System.out.println(Vechost);
+
+        BankAtm myBankAtm = new BankAtm("Samsung", myBank, myBankOffice, Vechost);
+        myBankAtm.addMoney(myBankAtm, 228322);
+        System.out.println(myBankAtm);
+
+        User Leva = new User("Вечность Лев", new Date(), "LevaBank228", myBank);
+
+        PaymentAccount pa = new PaymentAccount(Leva, myBank);
+        pa.addMoney(pa, 228322);
+        System.out.println(pa);
+
+        CreditAccount ca = new CreditAccount(Leva, myBank, Vechost, pa);
+        ca.setCreditSum(1337);
+        System.out.println(ca);
+
+
+        Leva.setPaymentAccount(pa);
+        Leva.setCreditAccount(ca);
+        System.out.println(Leva);
 
     }
 }
